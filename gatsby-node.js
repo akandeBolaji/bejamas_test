0c550237d8f4f8732d024e77e531bad448cbfe1a
akandeBolaji <akandbolajidanielofficial@gmail.com>
2019-12-01 16:23:06 +0100

    chore(create pages): add create pages to gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,60 @@
 const { createFilePath } = require('gatsby-source-filesystem')
 const { fmImagesToRelative } = require('gatsby-remark-relative-images')
+const path = require("path");
 
-exports.createPages = () => {}
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  return graphql(`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              path
+              templateKey
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      result.errors.forEach(e => console.error(e.toString()));
+      return Promise.reject(result.errors);
+    }
+
+    // Filter out the navbar and blogs so we don't create pages for those
+    const postOrPage = result.data.allMarkdownRemark.edges.filter(edge => {
+      if (edge.node.frontmatter.templateKey === "navbar") {
+        return false;
+      } else {
+        return !Boolean(edge.node.fields.slug.match(/^\/blog\/.*$/));
+      }
+    });
+
+    postOrPage.forEach(edge => {
+      let component, pathName;
+        pathName = edge.node.fields.slug;
+        component = path.resolve(`src/templates/${String(edge.node.frontmatter.templateKey)}.js`);
+      const id = edge.node.id;
+      createPage({
+        path: pathName,
+        component,
+        // additional data can be passed via context
+        context: {
+          id,
+        },
+      });
+    });
+  });
+};
+
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
