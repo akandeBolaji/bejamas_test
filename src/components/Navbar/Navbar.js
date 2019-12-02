033a5436b695880450ea2301f9ecc344b88963ca
akandeBolaji <akandbolajidanielofficial@gmail.com>
2019-12-02 09:52:52 +0100

    chore(navbar component): refactor navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import "./styles.scss";
+
+export const NavbarTemplate = ({ data }) => (
+  <nav className="navbar">
+    <div className="container  navbar-container">
+      {data.menuItems.length > 0 && (
+        <ul className="navbar-menu">
+          {data.menuItems.map(menuItem => (
+            <li key={menuItem.linkURL} className="navbar-menuItem">
+                <Link className="navbar-menuLink" to={menuItem.linkURL}>
+                    {menuItem.label}
+                </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  </nav>
+);
+
+const Navbar = props => {
+  if (!props.data) {
+    return null;
+  }
+  const data = props.data.edges[0].node.frontmatter;
+  return <NavbarTemplate data={data} />;
+};
+
+export { Navbar };
